04dbfdd80d17a33987da20e71dd8eafe874fec85
ayuvgr8 <ayuvgr8@gmail.com>
2021-10-24 04:48:47 +0530

    fc

---
+++ practice/oddeven.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cin>>n;
+    if(n%2==0){ //logic
+        cout<<"number is even";
+    }else{
+        cout<<"nnumber is odd";
+    }
+}
