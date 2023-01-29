404700098d8cacecb441897b17b0ef87ddb2c6bc
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2023-01-29 15:17:30 +0000

    p1

---
+++ .ccls-cache/@home@runner@practice/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using  namespace std;
+
+int main() {
+  // int a =5;
+  // cout << "Namsate Duniyaaa \n";
+  // cout<< a<< endl;
+
+  
+  // int a = 'a';
+  // cout << a<< endl;
+
+  // char ch = 1234;
+  // cout<< ch;
+  // float a =2.0/5;
+  // cout<< a;
+
+  
+  
+  //a=cin.get();
+  
+  //cout<< "value of a is :" <<a<<endl;
+  //  cout<< "The Value of a and b is "<<a <<" "<<b<<endl;
+  // if(a>b){
+
+  //   cout<< " A is bigger "<<endl;
+  // } if (b>a) {
+  //  cout<< "B is bigger"<< endl;
+  // }
+  int n;
+  cin>>n;
+  int i=2;
+  while (i<n){
+    n%i= 
+  }
+
+  
+  }
